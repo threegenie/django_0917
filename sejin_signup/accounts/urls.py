42b00238793192fd75a710591844f2b451356e2d
threegenie <63702924+threegenie@users.noreply.github.com>
2020-09-18 18:07:48 +0900

    add files

---
+++ sejin_signup/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('signup/', views.signup, name='signup'),
+        path('login/', views.login, name='login'),
+        path('logout/', views.logout, name='logout'),
+        path('', views.index, name='index'),
+        path('userpage', views.userpage, name='userpage'),
+        path('activate/<str:uidb64>/<str:token>/', views.activate, name="activate"),
+]
